f1aa306c216c44def268e90a84e7349c7b6cc5d5
Your Name <robocoders3301@gmail.com>
2026-07-06 23:21:44 +0100

    fix NotificationBell import path

--- resources/js/Layouts/TeamLayout.vue
+++ resources/js/Layouts/TeamLayout.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { Link, usePage } from '@inertiajs/vue3'
 import { computed, ref, watch } from 'vue'
-import NotificationBell from '@/components/NotificationBell.vue'
+import NotificationBell from '@/Components/NotificationBell.vue'
 import Swal from 'sweetalert2'
 
 const page = usePage()
