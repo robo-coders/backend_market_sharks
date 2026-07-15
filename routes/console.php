8aaae8d51cf31317f81d34dff1508473e716ff35
Your Name <robocoders3301@gmail.com>
2026-07-15 17:45:55 +0100

    fix: correct signals:monitor schedule to everyFiveMinutes for shared hosting cron

--- routes/console.php
+++ routes/console.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('subscriptions:expire')->dailyAt('00:00');
 Schedule::command('subscriptions:remind-expiring')->dailyAt('09:00');
-Schedule::command('signals:monitor')->everyMinute()->withoutOverlapping();
+Schedule::command('signals:monitor')->everyFiveMinutes()->withoutOverlapping();
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
